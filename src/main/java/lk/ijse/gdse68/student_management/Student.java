3a7a78b26ab8d4edb2bde9c1527b783f7ed33964
amliyanage <ashenmliyanage@gmail.com>
2024-07-14 10:12:40 +0530

    Save Method Developed

--- src/main/java/lk/ijse/gdse68/student_management/Student.java
+++ src/main/java/lk/ijse/gdse68/student_management/Student.java
@@ -1,11 +1,8 @@
 package lk.ijse.gdse68.student_management;
 
-import jakarta.json.Json;
-import jakarta.json.JsonArray;
-import jakarta.json.JsonObject;
-import jakarta.json.JsonReader;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,38 +11,71 @@ import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.gdse68.student_management.Dto.StudentDto;
 import lk.ijse.gdse68.student_management.util.Utils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.rmi.server.UID;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.UUID;
 
 @WebServlet(urlPatterns = "/student")
 public class Student extends HttpServlet {
 
     private Connection connection;
 
+    private static final String SAVE_STUDENT = "INSERT INTO STUDENT (Stu_Id, Name, email, level) VALUES (?, ?, ?, ?)";
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        try {
+            var dbClass = getServletContext().getInitParameter("db-class");
+            var dbUrl = getServletContext().getInitParameter("db-url");
+            var dbUsername = getServletContext().getInitParameter("db-username");
+            var dbPassword = getServletContext().getInitParameter("db-password");
+            Class.forName(dbClass);
+            this.connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new ServletException(e);
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO, Get Student
+        // TODO: Get Student
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO Save Student
-        if ( req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json") ){
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
 
-        Jsonb jsonb = JsonbBuilder.create();
-        StudentDto studentDto = jsonb.fromJson(req.getReader(), StudentDto.class);
-        studentDto.setId(Utils.generateId());
+        try (var reader = req.getReader(); var writer = resp.getWriter()) {
+            Jsonb jsonb = JsonbBuilder.create();
+            StudentDto studentDto = jsonb.fromJson(reader, StudentDto.class);
+            studentDto.setId(Utils.generateId());
 
-        resp.setContentType("application/json");
-        jsonb.toJson(studentDto,resp.getWriter());
+            try (PreparedStatement pstm = connection.prepareStatement(SAVE_STUDENT)) {
+                pstm.setString(1, studentDto.getId());
+                pstm.setString(2, studentDto.getName());
+                pstm.setString(3, studentDto.getEmail());
+                pstm.setString(4, studentDto.getLevel());
+
+                boolean success = pstm.executeUpdate() > 0;
+
+                if (success) {
+                    resp.setStatus(HttpServletResponse.SC_CREATED);
+                    writer.write("Save Student Success");
+                } else {
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                    writer.write("Save Student Failed");
+                }
+            }
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -55,20 +85,16 @@ public class Student extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO , Delete Student
+        // TODO: Delete Student
     }
 
     @Override
-    public void init() throws ServletException {
-        //TODO, Initialize
+    public void destroy() {
         try {
-            var dbClass = getServletContext().getInitParameter("com.mysql.cj.jdbc.Driver");
-            var dbUrl = getServletContext().getInitParameter("jdbc:mysql://localhost:3306/student_management");
-            var dbUsername = getServletContext().getInitParameter("root");
-            var dbPassword = getServletContext().getInitParameter("Ijse@2024");
-            Class.forName(dbClass);
-            this. connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-        } catch (ClassNotFoundException | SQLException e) {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }

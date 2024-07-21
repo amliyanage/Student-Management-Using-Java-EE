b8edeb951b471f31024d45ea6eca7603f868083f
amliyanage <ashenmliyanage@gmail.com>
2024-07-21 11:57:22 +0530

    cors error resolved

---
+++ src/main/java/lk/ijse/gdse68/student_management/Filter/CrossFilter.java
@@ -0,0 +1,28 @@
+package lk.ijse.gdse68.student_management.Filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CrossFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        var origin = getServletContext().getInitParameter("origin");
+
+        if (origin != null) {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+        }
+
+        chain.doFilter(req, res);
+
+    }
+}
